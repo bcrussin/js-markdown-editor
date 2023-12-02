3afc1af2754da40cc6371a6945f08d93ab8ae0bf
Barrett Crusse <cvillebear@gmail.com>
2023-12-01 22:22:45 -0500

    Populate guide using API

--- script/guide.js
+++ script/guide.js
@@ -1,4 +1,4 @@
-const CHEAT_SHEET_URL = "https://www.datamuse.com/words?ml=ringing+in+the+ears";
+const CHEAT_SHEET_URL = "https://corsproxy.io/?https://www.markdownguide.org/api/v1/cheat-sheet.json";
 
 const guideContent = document.getElementById("guide-content");
 
@@ -7,12 +7,49 @@ this.onload = () => {
 };
 
 function loadCheatSheet() {
-	let request = new XMLHttpRequest();
-	request.addEventListener("load", parseData);
-	request.open("GET", "https://www.markdownguide.org/api/v1/cheat-sheet.json");
-	request.send();
+	fetch(CHEAT_SHEET_URL)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(parseCheatSheet(data["cheat_sheet"]));
+		});
 }
 
-function parseData() {
-	console.log(1);
+function parseCheatSheet(data) {
+	guideContent.innerHTML = "";
+
+	for (let sectionObject of data) {
+		let sectionName = Object.keys(sectionObject)[0];
+		let sectionItems = Object.values(sectionObject)[0];
+
+		sectionName = sectionName.replace("_", " ");
+		sectionName = sectionName
+			.toLowerCase()
+			.split(" ")
+			.map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+			.join(" ");
+
+		let sectionDiv = document.createElement("div");
+		let header = document.createElement("h2");
+		header.innerHTML = sectionName;
+		sectionDiv.appendChild(header);
+
+		let sectionContent = document.createElement("div");
+		sectionContent.className = "section-content";
+		sectionDiv.appendChild(sectionContent);
+
+		console.log(sectionName);
+		for (let item of sectionItems) {
+			console.log(item);
+			let title = document.createElement("h4");
+			title.innerHTML = item["element"];
+
+			let syntax = document.createElement("code");
+			syntax.innerHTML = item["syntax"];
+
+			sectionContent.appendChild(title);
+			sectionContent.appendChild(syntax);
+		}
+
+		guideContent.appendChild(sectionDiv);
+	}
 }
